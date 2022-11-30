54c29c1fe82de3fee83c8fc2dd626ac45170847c
KareemE125 <kareemezzat.125@gmail.com>
2022-11-30 16:28:27 +0200

    comment add

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -9,7 +9,7 @@ let store = configureStore({
     reducer:{ 
         notes: notesReducer,
     },
-    // serializableCheck: false is used because we are dealing with Note class which is non-serializable
+    // serializableCheck: false is used because we are dealing with Classes (Note class) which is non-serializable
     middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 });
 
